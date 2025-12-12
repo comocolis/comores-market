6c76743dbf02276ae74a1a84dd610cc7bfc9d606
sisco6 <abdesisco1@gmail.com>
2025-12-12 13:56:49 +0100

    as

--- src/app/utils/supabase/server.ts
+++ src/app/utils/supabase/server.ts
@@ -18,9 +18,7 @@ export async function createClient() {
               cookieStore.set(name, value, options)
             )
           } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+            // Ignorer si appelé depuis un Server Component
           }
         },
       },
