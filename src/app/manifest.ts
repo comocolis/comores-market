1bee3ea16a23ab9f941a84f24aac3c0e40eb54df
sisco6 <abdesisco1@gmail.com>
2025-12-15 16:57:00 +0100

    manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,24 +4,35 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Comores Market',
     short_name: 'ComoresMarket',
-    description: 'Achat et vente facile aux Comores',
+    description: 'Vente et achat aux Comores',
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
-    theme_color: '#22c55e',
+    theme_color: '#10b981',
     icons: [
       {
         src: '/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
-        // Avec la config désactivée, cette ligne passera sans bloquer !
-        purpose: 'any maskable', 
+        purpose: 'any',
+      },
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
       },
       {
         src: '/android-chrome-512x512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'any',
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   }
